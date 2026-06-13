a8a5ad488fed739b780300ae3768738cbf23cfe9
omprakash8788 <omprakashkumar552@gmail.com>
2026-06-13 17:47:36 +0530

    ch4

--- code/library/django_project/settings.py
+++ code/library/django_project/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'books.apps.BooksConfig'
+    'rest_framework',
+    'books.apps.BooksConfig',
+    'apis.apps.ApisConfig',
+
 ]
 
 MIDDLEWARE = [

d3a49f1c53c5f43a4231bbd551d609e62e1798a4
omprakash8788 <omprakashkumar552@gmail.com>
2026-06-15 19:57:07 +0530

    py basic

---
+++ basic-py/Chapter1.py
@@ -0,0 +1,59 @@
+print("Hello")
+
+# Chap2 -> Topic - Variable and Data Types
+
+# Assigning a value to a variable
+message = "Hello, world!"
+number = 42
+pi_value = 3.14
+
+# Printing the value 
+print(message, number, pi_value) #I am lazy
+
+
+# Common data types
+# Integers(), Floating Point numbers(), String , Booleans, List, Tuples, Dictionaries
+
+integer =10
+floating_point = 10.5
+string="Python programming"
+boolean=True
+list_example=[1,2,3,4]
+tuple_example=(1,2,3,4)
+dictionary_exmple={"name":"john", "age":40}
+
+# Displaying the data types of each variables
+print(type(integer))
+print(type(floating_point))
+print(type(string))
+print(type(boolean))
+print(type(list_example))
+print(type(tuple_example))
+print(type(dictionary_exmple)) #Now i am not lazy
+
+#Type Conversion
+
+# Converting integer to float
+num_int=10
+nums_float=float(num_int)
+print(nums_float)
+
+# Converting  float to integer
+nums_float=9.7
+num_int=int(nums_float)
+print(num_int)
+
+# Converting integer to string
+num_int=300
+num_str=str(num_int)
+print(num_str)
+
+# Converting string to integer
+
+num_str="800"
+num_int=int(num_str)
+print(num_int)
+
+
+
+

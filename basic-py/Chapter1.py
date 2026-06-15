688ba3f5e9ccd5424a0d1f4f93a2cd6664d94e3b
omprakash8788 <omprakashkumar552@gmail.com>
2026-06-15 21:17:32 +0530

    chap3

--- basic-py/Chapter1.py
+++ basic-py/Chapter1.py
@@ -55,5 +55,94 @@ num_int=int(num_str)
 print(num_int)
 
 
+# Chap -2 (Operators and Expressions)
+
+#Additions
+addition= 5 +3
+print("additions", addition)
+
+#Sub 
+subtraction = 5 - 3
+print("sub", subtraction)
+
+#Mutliplication
+multiplication = 6 * 6
+print("multi", multiplication)
+
+# Division
+division = 5/3
+print("Division", division)
+
+
+# Modulus (remainder)
+modulus = 5 % 3
+print("Modulus:", modulus)
+
+# Exponentiation (power)
+exponentiation = 5 ** 3
+print("Exponentiation:", exponentiation) 
+
+
+# Comparison Operators 
+# Equal to
+equal = 5==3
+print(equal) 
+
+# Not equal 
+not_equal = 5!=4
+print(not_equal)
+
+# Greater than
+greater_than = 5 > 3
+print("Greater Than:", greater_than)
+
+# Less than
+less_than = 5 < 3
+print("Less Than:", less_than)
+
+
+# Greater than or equal to
+greater_than_equal = 5 >= 3
+print("Greater Than or Equal To:", greater_than_equal)
+
+# Less than or equal to
+less_than_equal = 5 <= 3
+print("Less Than or Equal To:", less_than_equal)
+
+#  Logical Operators
+
+x = True
+y = False
+
+# Logical AND
+print("x and y : ", x and y )
+
+# Logical OR
+print("x or y : ", x or y )
+
+# Logical NOT
+print("not x:", not x) 
+
+
+# Logical operators often combine multiple comparison operations:
+a = 10 
+b=12
+c=5
+
+# Combining comparison and logical operators
+
+result = (a > b) and (a > c)
+print(result)
+
+result =(a > b) or (a > c)
+print(result)
+
+result = not(a > b)
+print(result)
+
+
+
+
+
 
 

a067449f5b406d666e16265440639168311ff104
omprakash8788 <omprakashkumar552@gmail.com>
2026-06-27 23:46:37 +0530

    updating api version 2

--- task_manager_app/operations.py
+++ task_manager_app/operations.py
@@ -1,7 +1,7 @@
 # CRUD Operation
 import csv
 from typing import Optional
-from models import Task, TaskWithID
+from models import Task, TaskWithID, TaskV2WithID
 
 DATABASE_FILENAME="tasks.csv"
 column_fields=[
@@ -105,3 +105,9 @@ def remove_task(id: int) -> bool:
 
         del dict_task_without_id["id"]
         return Task(**dict_task_without_id)
+    
+
+def read_all_tasks_v2()-> list[TaskV2WithID]:
+    with open(DATABASE_FILENAME) as csvfile:
+        reader = csv.DictReader(csvfile,)
+        return [TaskV2WithID(**row) for row in reader]
